35146286d0da9c718c6ec356ff5fbfd01f29449c
mislam <md.islam@factset.com>
2020-04-02 19:13:32 -0400

    pinch

--- client-app/components/globe.vue
+++ client-app/components/globe.vue
@@ -243,8 +243,9 @@ export default {
         .on('drag', this.onDrag)
         .on('end', this.onDragEnd)
       )
-      .call(d3.zoom)
-      .on('wheel.zoom', this.onZoom)
+      .call(d3.zoom()
+        .on('zoom', this.onZoom)
+      )
 
     console.log('Setting draw loop timer')
     this.isRotating = true
