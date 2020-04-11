6ff4bb0732bd695fbc7218701d9d08f0ac2dba02
Tanvir <tanvirislam.cs@gmail.com>
2020-04-11 03:35:32 -0400

    url fixed

--- client-app/store/urls.js
+++ client-app/store/urls.js
@@ -2,11 +2,11 @@ export const state = () => ({
   url: {
     worldMapData: 'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json',
     covidData: {
-      countriesConfirmed: 'api/countries/confirmed',
-      countriesDeaths: 'api/countries/deaths',
-      countriesRecovered: 'api/countries/recovered',
-      global: 'api/global',
-      countrySummaryBase: 'api/summary'
+      countriesConfirmed: 'https://covid-athenaeum.herokuapp.com/api/countries/confirmed',
+      countriesDeaths: 'https://covid-athenaeum.herokuapp.com/api/countries/deaths',
+      countriesRecovered: 'https://covid-athenaeum.herokuapp.com/api/countries/recovered',
+      global: 'https://covid-athenaeum.herokuapp.com/api/global',
+      countrySummaryBase: 'https://covid-athenaeum.herokuapp.com/api/summary'
     }
   }
 })
