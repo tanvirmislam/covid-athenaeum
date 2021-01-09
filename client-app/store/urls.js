f24c21b6e4f8a1c9731a1f97fecc64ea0dee2d7f
Tanvir Islam <tanvirmislam@Tanvir-Macbook-Pro.local>
2021-01-09 01:44:52 -0500

    updated base url path

--- client-app/store/urls.js
+++ client-app/store/urls.js
@@ -1,12 +1,14 @@
+const BASE_URL = 'https://covid-athenaeum.herokuapp.com'
+
 export const state = () => ({
   url: {
     worldMapData: 'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json',
     covidData: {
-      countriesConfirmed: `${process.env.COVID_WEBAPP_BASE_URL}/api/countries/confirmed`,
-      countriesDeaths: `${process.env.COVID_WEBAPP_BASE_URL}/api/countries/deaths`,
-      countriesRecovered: `${process.env.COVID_WEBAPP_BASE_URL}/api/countries/recovered`,
-      global: `${process.env.COVID_WEBAPP_BASE_URL}/api/global`,
-      countrySummaryBase: `${process.env.COVID_WEBAPP_BASE_URL}/api/summary`
+      countriesConfirmed: `${BASE_URL}/api/countries/confirmed`,
+      countriesDeaths: `${BASE_URL}/api/countries/deaths`,
+      countriesRecovered: `${BASE_URL}/api/countries/recovered`,
+      global: `${BASE_URL}/api/global`,
+      countrySummaryBase: `${BASE_URL}/api/summary`
     }
   }
 })
